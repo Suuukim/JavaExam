d34f6c9c0eeb109d9ab027df88367c14be1190ee
Suuukim <toki159@naver.com>
2022-05-30 17:07:11 +0900

    백준 자바 문제풀이

---
+++ src/koreait/day0127/Main.java
@@ -0,0 +1,5 @@
+package koreait.day0127;
+
+public class Main {
+
+}
---
+++ src/koreait/day0127/Main_1000.java
@@ -0,0 +1,15 @@
+package koreait.day0127;
+
+import java.util.Scanner;
+
+public class Main_1000 {
+	public static void main(String[] args) {
+	//백준 1000번 문제
+	//문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+	
+	Scanner sc = new Scanner(System.in);	
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	System.out.println(a + b);
+	}
+}
