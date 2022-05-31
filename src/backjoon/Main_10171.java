4bf5dedf237df32c9b4a364c12a24d1cd58aa425
Suuukim <toki159@naver.com>
2022-05-31 17:00:03 +0900

    고양이 만들기

---
+++ src/backjoon/Main_10171.java
@@ -0,0 +1,14 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Main_10171 {
+	public static void main(String[] args) {
+		//백준 10171번 문제
+		//문제 : 고양이를 출력한다.
+	    System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+	}
+}
