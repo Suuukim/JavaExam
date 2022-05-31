2f8647e275a4e7431691adae2af627e86f9338a8
Suuukim <toki159@naver.com>
2022-05-31 16:59:30 +0900

    나머지 구하는 문제

--- src/backjoon/Main_10869.java
+++ src/backjoon/Main_10869.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main_10869 {
 	public static void main(String[] args) {
 		//백준 10869번 문제
-		//문제 : 두 자연수 A와 B가 주어진다. 이떄, A+B, A-B, A*B, A/B(몫),
+		//문제 : 두 자연수 A와 B가 주어진다. 이떄, 
+		//		A+B, A-B, A*B, A/B(몫),
 		//     A%B(나머지)를 출력하는 프로그램을 작성하시오.
 		
 		Scanner sc = new Scanner(System.in);
