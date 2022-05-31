9403cc18c0f1436b6f5392b4e85f42ff1f58e6ae
Suuukim <toki159@naver.com>
2022-05-31 17:02:31 +0900

    개를 출력하기

---
+++ src/backjoon/Main_10172.java
@@ -0,0 +1,16 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Main_10172{
+	public static void main(String[] args) {
+		//백준 : 10172번 문제
+		//문제 : 개를 출력한다.
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
