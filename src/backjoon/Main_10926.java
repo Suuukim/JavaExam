47164bdf9bd31cce6bd3843c213aa15d5229e8a7
Suuukim <toki159@naver.com>
2022-06-02 16:49:28 +0900

    준하의 놀람을 출력, 놀람은 아이디 뒤에 ??!를 붙여서 나타냄

---
+++ src/backjoon/Main_10926.java
@@ -0,0 +1,15 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Main_10926 {
+	public static void main(String[] args) {
+		//백준 10926번 문제
+		//문제 : 첫째 줄에 준하의 놀람을 출력한다. 놀람은 아이디 뒤에 ??!를 붙여서 나타낸다.
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.next();
+		System.out.println(name + "??!");
+		
+	}
+}
