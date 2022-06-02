d15cd4321cafbb80162886faa2c57d6883abcf1f
Suuukim <toki159@naver.com>
2022-06-02 16:41:38 +0900

    첫째 줄에 A+B를 출력하기

---
+++ src/backjoon/Main_2558.java
@@ -0,0 +1,16 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Main_2558 {
+	public static void main(String[] args) {
+		//백준 2558번 문제
+		//문제 : 첫째 줄에 A+B를 출력한다.
+		
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A+B);
+	}
+}
