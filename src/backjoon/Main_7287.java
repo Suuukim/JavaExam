690a5718e044a956491949c7ddb7be4374dc9b83
Suuukim <toki159@naver.com>
2022-06-02 16:42:37 +0900

    자신이 맞은 문제의 수, 아이디를 출력하기.

---
+++ src/backjoon/Main_7287.java
@@ -0,0 +1,12 @@
+package backjoon;
+
+public class Main_7287 {
+	public static void main(String[] args) {
+		//백준 7287번 문제
+		//문제 : 첫 줄에 자신이 맞은 문제의 수, 둘째 줄에 아이디를 출력한다.
+		
+		System.out.println(11);
+		System.out.println("toki159");
+		
+	}
+}
