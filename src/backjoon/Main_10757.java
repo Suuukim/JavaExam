2a4c990d6c5d2ce92a962850548bbef854a5428b
Suuukim <toki159@naver.com>
2022-06-03 15:44:48 +0900

    BigInteger 클래스 사용

---
+++ src/backjoon/Main_10757.java
@@ -0,0 +1,13 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Main_10757 {
+	public static void main(String[] args) {
+		//백준 10757번 문제
+		//문제 : 첫째 줄에 A+B를 출력한다.
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextBigInteger().add(sc.nextBigInteger()));
+	}
+}
