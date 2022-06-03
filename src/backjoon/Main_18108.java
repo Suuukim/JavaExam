86e0dc31496c6c14007d576190d377ccc1c3a3d6
Suuukim <toki159@naver.com>
2022-06-03 15:36:45 +0900

    불기연도를 서기연도로 변환

---
+++ src/backjoon/Main_18108.java
@@ -0,0 +1,15 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Main_18108 {
+	public static void main(String[] args) {
+		//백준 18108번 문제
+		//문제 : 불기 연도를 서기연도로 변환한 결과를 출력한다.
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(a - 543);
+		
+	}
+}
